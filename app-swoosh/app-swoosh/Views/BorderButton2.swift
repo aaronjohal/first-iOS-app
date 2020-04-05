2af82220f51b43afbf2db96e9af6045e5cc0c42c
Aaron Johal <aaronjohal@gmail.com>
2020-04-05 14:20:10 +0100

    button class 2

---
+++ app-swoosh/app-swoosh/Views/BorderButton2.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton2.swift
+//  app-swoosh
+//
+//  Created by Aaron Johal on 05/04/2020.
+//  Copyright © 2020 Aaron Johal. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton2: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

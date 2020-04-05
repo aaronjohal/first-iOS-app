2aeac2380523c317cd326efe997f41d122d9bc5b
Aaron Johal <aaronjohal@gmail.com>
2020-04-05 14:44:40 +0100

    UI button highlight changes

--- app-swoosh/app-swoosh/Views/BorderButton2.swift
+++ app-swoosh/app-swoosh/Views/BorderButton2.swift
@@ -10,12 +10,17 @@ import UIKit
 
 class BorderButton2: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+        
     }
-    */
+    
+    
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? .black : .gray        }
+    }
+    
 
+    
 }
